3eb0764552430eb99a7401a325d4fb17ac14b2ab
Muhmmd-Usama <mummdusama@gmail.com>
2022-07-31 16:59:46 +0200

    Create flip blocks function for cards

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const blockCard = document.querySelectorAll('.game-block');
+
 // ########### Splash screen #########################
 document.querySelector('.control-button span').onclick = () => {
   let myName = prompt('Enter Your Name');
-  console.log(myName);
   if (myName == '') {
     myName = 'Unknowen';
     document.querySelector('.name span').textContent = myName;
@@ -11,7 +12,7 @@ document.querySelector('.control-button span').onclick = () => {
   document.querySelector('.control-button').remove();
 };
 
-// ###########  #########################
+// ########### Shuffle Function  #########################
 
 let shuffle = (arr) => {
   let currentNum = arr.length,
@@ -23,7 +24,6 @@ let shuffle = (arr) => {
     temp = arr[currentNum];
     arr[currentNum] = arr[randomNum];
     arr[randomNum] = temp;
-
     // Swaping values without temp var
     //   arr[currentNum] = arr[currentNum] + arr[randomNum];
     //   arr[randomNum] = arr[currentNum] - arr[randomNum];
@@ -32,20 +32,47 @@ let shuffle = (arr) => {
   return arr;
 };
 
-let duration = 1000;
+// ########### Stop Clicking Function #########################
+
+let stopClicking = () => {
+  // document
+  //   .querySelector('.game-block')
+  //   .addEventListener('click', (e) => e.stopPropagation());
+  blocksContainer.classList.add('no-clicking');
+  setTimeout(() => blocksContainer.classList.remove('no-clicking'), 1000);
+};
+
+// ########### Flip Function #########################
+
+let flipBlock = (selectedBlock) => {
+  selectedBlock.classList.add('is-flipped');
+
+  let flippedBlocks = blocks.filter((flippedBlock) =>
+    flippedBlock.classList.contains('is-flipped')
+  );
+  if (flippedBlocks.length === 2) {
+    console.log('Two Flipped');
+
+    // Stop Clicking
+    stopClicking();
+  }
+};
+
+// let duration = 1000;
 
 let blocksContainer = document.querySelector('.memory-game-container');
 
-// Creat Array from game block
+// Creat Array from blocks Container
 let blocks = Array.from(blocksContainer.children);
 
 // Create a range of keys according to blocks numbers
 let orderRange = [...Array(blocks.length).keys()];
 
-console.log(orderRange);
 shuffle(orderRange);
-console.log(orderRange);
 
 blocks.forEach((block, index) => {
   block.style.order = orderRange[index];
+  block.addEventListener('click', () => {
+    flipBlock(block);
+  });
 });
